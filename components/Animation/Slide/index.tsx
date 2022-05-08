24af231698b488af78e89c5e0f2c501c95cb99a0
alfonsus20 <alfonschandrawan@gmail.com>
2022-05-08 22:09:54 +0700

    feat: add navbar animation

--- components/Animation/Slide/index.tsx
+++ components/Animation/Slide/index.tsx
@@ -20,7 +20,7 @@ const Slide = ({ children, direction }: SlideProps) => {
       opacity: 1,
       transition: {
         ease: [0.6, 0.01, -0.05, 0.95],
-        duration: 1,
+        duration: 1.2,
       },
     },
   };
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import HamburgerMenu from "react-hamburger-menu";
+import { ChakraBox } from "../Animation";
 
 type NavbarMobileProps = {
   isSidebarOpen: boolean;
@@ -13,6 +14,18 @@ type NavbarMobileProps = {
 const Navbar = ({ isSidebarOpen, toggleSidebar }: NavbarMobileProps) => {
   const { pathname } = useRouter();
   const [isNavbarBgShown, setIsNavbarBgShown] = useState<boolean>(false);
+
+  const animation = {
+    initial: {
+      y: -100,
+      opacity: 0,
+    },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: { ease: "easeInOut", duration: 1, delay: 1.5 },
+    },
+  };
 
   useEffect(() => {
     const watchPageScroll = () =>
@@ -25,92 +38,99 @@ const Navbar = ({ isSidebarOpen, toggleSidebar }: NavbarMobileProps) => {
       });
 
     watchPageScroll();
+
     return () => {
       document.removeEventListener("scroll", watchPageScroll);
     };
   });
 
   return (
-    <Flex
-      as="header"
-      justifyContent="space-between"
-      alignItems="center"
-      px={8}
-      py={4}
+    <ChakraBox
+      variants={animation}
       pos={pathname === "/" ? "fixed" : "sticky"}
       top={0}
       left={0}
       right={0}
       zIndex={20}
-      shadow={pathname === "/" && !isNavbarBgShown ? "none" : "md"}
-      bgColor={{
-        base: "white",
-        md: pathname === "/" && !isNavbarBgShown ? "transparent" : "white",
-      }}
-      transition="background 0.3s ease-out"
+      initial="initial"
+      animate="animate"
     >
-      <Link href="/" passHref>
-        <Box as="a">
-          <Image src="/logo.png" width={80} height={50} alt="logo" />
-        </Box>
-      </Link>
       <Flex
-        columnGap="4"
-        color={pathname === "/" && !isNavbarBgShown ? "white" : "black"}
-        display={{ base: "none", md: "flex" }}
+        as="header"
+        justifyContent="space-between"
+        alignItems="center"
+        px={8}
+        py={4}
+        shadow={pathname === "/" && !isNavbarBgShown ? "none" : "md"}
+        bgColor={{
+          base: "white",
+          md: pathname === "/" && !isNavbarBgShown ? "transparent" : "white",
+        }}
+        transition="background 0.3s ease-out"
       >
         <Link href="/" passHref>
-          <Box px={2} as="a">
-            Beranda
+          <Box as="a">
+            <Image src="/logo.png" width={80} height={50} alt="logo" />
           </Box>
         </Link>
-        <Link href="/berita" passHref>
-          <Box px={2} as="a">
-            Berita
-          </Box>
-        </Link>
-        <Link href="/mahasiswa" passHref>
-          <Box px={2} as="a">
-            Mahasiswa
-          </Box>
-        </Link>
-        <Link href="/alumni" passHref>
-          <Box px={2} as="a">
-            Alumni
-          </Box>
-        </Link>
-        <Link href="/galeri" passHref>
-          <Box px={2} as="a">
-            Galeri
-          </Box>
-        </Link>
-      </Flex>
-      <Link href="/login" passHref>
-        <Button
-          as="a"
-          colorScheme={
-            pathname === "/" && !isNavbarBgShown ? "whiteAlpha" : "red"
-          }
-          _hover={{ bgColor: "red.400" }}
-          display={{ base: "none", md: "inherit" }}
+        <Flex
+          columnGap="4"
+          color={pathname === "/" && !isNavbarBgShown ? "white" : "black"}
+          display={{ base: "none", md: "flex" }}
         >
-          Login
-        </Button>
-      </Link>
-      <Box display={{ base: "inherit", md: "none" }}>
-        <HamburgerMenu
-          isOpen={isSidebarOpen}
-          menuClicked={toggleSidebar}
-          width={18}
-          height={15}
-          strokeWidth={2}
-          rotate={0}
-          color="black"
-          borderRadius={2}
-          animationDuration={0.5}
-        />
-      </Box>
-    </Flex>
+          <Link href="/" passHref>
+            <Box px={2} as="a">
+              Beranda
+            </Box>
+          </Link>
+          <Link href="/berita" passHref>
+            <Box px={2} as="a">
+              Berita
+            </Box>
+          </Link>
+          <Link href="/mahasiswa" passHref>
+            <Box px={2} as="a">
+              Mahasiswa
+            </Box>
+          </Link>
+          <Link href="/alumni" passHref>
+            <Box px={2} as="a">
+              Alumni
+            </Box>
+          </Link>
+          <Link href="/galeri" passHref>
+            <Box px={2} as="a">
+              Galeri
+            </Box>
+          </Link>
+        </Flex>
+        <Link href="/login" passHref>
+          <Button
+            as="a"
+            colorScheme={
+              pathname === "/" && !isNavbarBgShown ? "whiteAlpha" : "red"
+            }
+            _hover={{ bgColor: "red.400" }}
+            display={{ base: "none", md: "inherit" }}
+          >
+            Login
+          </Button>
+        </Link>
+        <Box display={{ base: "inherit", md: "none" }}>
+          <HamburgerMenu
+            isOpen={isSidebarOpen}
+            menuClicked={toggleSidebar}
+            width={18}
+            height={15}
+            strokeWidth={2}
+            rotate={0}
+            color="black"
+            borderRadius={2}
+            animationDuration={0.5}
+          />
+        </Box>
+      </Flex>
+    </ChakraBox>
   );
 };
 
