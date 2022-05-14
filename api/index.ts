4e17bec30d693db06328433353af9d325cada096
alfonsus20 <alfonschandrawan@gmail.com>
2022-05-14 22:10:26 +0700

    fix: change to deployment url

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookie from "js-cookie";
 
 export default axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://ddb-backend.herokuapp.com",
   headers: {
     Authorization: `Bearer ${Cookie.get("token")}`,
   },
